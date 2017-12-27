7805b2556f3ec40d22a392c19b70b796ca811880
root <root@contoso.local>
2017-12-27 15:31:19 -0800

    c

--- update-tools.sh
+++ update-tools.sh
@@ -1,6 +1,18 @@
+apt install figlet
+apt install variety
 #powershell empire
+echo "Detecting Powershell Empire..."
 if [ ! -d "/opt/Empire" ]; then
 git clone https://github.com/EmpireProject/Empire.git /opt/Empire
 else
-echo "Empire installed...."
+echo "Installed!!"
 fi
+
+echo "Detecting CrackMap Exec..."
+if [ ! -d "/opt/CrackMapExec" ]; then
+git clone https://github.com/byt3bl33d3r/CrackMapExec.git /opt/CrackMapExec
+else
+echo "Installed!!"
+fi
+
+
