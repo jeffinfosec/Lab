7955f912ac223a93e265624da66e2938c2d9bfe3
Jeffrey Li <Jeffrey Li>
2018-04-19 09:36:25 -0700

    c

--- programming/python/practicepython.org/06StringListPalidrome.py
+++ programming/python/practicepython.org/06StringListPalidrome.py
@@ -1,7 +1,8 @@
+#!/usr/bin/env python
 wrd=raw_input("Please enter a word: ")
 print ("Word entered: " + wrd)
 wrd=str(wrd)
-rvs = wrd[::-1]
-print (rvs)
+reverse = wrd[::-1]
+print (reverse)
 
 raw_input("Press Enter to continue...")
---
+++ programming/python/practicepython.org/practice.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# print statement
+print("Type a number odd or event")
+num1 = int(raw_input("enter first numer"))
+num2 = int(raw_input("enter second number"))
+sum = num1 + n
