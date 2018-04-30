5fe62713563e48f1f93154ca0d0c8546c37ae195
root <root@localhost.localdomain>
2018-04-30 06:55:03 -0700

    -c

--- programming/python/practicepython.org/02_oddeven.py
+++ programming/python/practicepython.org/02_oddeven.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python
 print("Exercise 2: Odd or Even -")
-
 num = int(raw_input("Enter a number: "))
 mod = num % 2
-
 print(mod)
-
 if mod == 1:
 	print("You typed in a odd number")
 else: 
 	print("You typed in an even number")
-
 raw_input("Press Enter to continue...")	
--- programming/python/practicepython.org/practice.py
+++ programming/python/practicepython.org/practice.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
-# print statement
-print("Type a number odd or event")
-num1 = int(raw_input("enter first numer"))
-num2 = int(raw_input("enter second number"))
-sum = num1 + n
+def regArgFunc(empname):
+	print (empname)
+	print type(empname)
+	return;
+
+regArgFunc("1")
+
+regArgFunc("Jeffrey Li")
