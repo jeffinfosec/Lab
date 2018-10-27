8025753ee4a4650275b0b532779103d49b5e6f36
root <root@localhost.localdomain>
2018-10-27 10:09:37 -0700

    c

---
+++ programming/c/filehandling.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	char str[1000];
+	FILE *fptr;
+	char fname[20]="test.txt";
+
+}
---
+++ programming/c/print-name.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(void){
+	printf("Jeffrey Li\n");
+	printf("DOB\n");
+	printf("Mobile\n");
+	return 0;
+}
