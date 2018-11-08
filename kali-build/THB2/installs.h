bd623576d3338ee7809b05824665f23063b831aa
Jeffrey Li <jli@localhost.localdomain>
2018-11-08 10:31:16 -0800

    cc

---
+++ kali-build/THB2/installs.h
@@ -0,0 +1,1 @@
+command "git clone https://
---
+++ kali-build/py-readwritedemo.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+print("# file write / read demo")
+
+def p(str):
+	print(str)
+
+def main():
+        p("creating file")
+	f = open("guru99.txt","w+")
+# open() built in function to open files
+wd
+	p(f)
+	print("# file write / read demo")
+
+main()
